5c6fa06acd9e1dbb19459d3b7bf57bb80a567508
leegentle <gentlee.dev@gmail.com>
2025-07-03 23:19:35 +0900

    feat : 확장 단계 추가 진행중

--- content.js
+++ content.js
@@ -1,47 +1,81 @@
+const MAX_LEVEL = 5;
+const MIN_WIDTH = 405;
+
 // 메시지 리스너
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    const isExtend = request.action === 'extend';
-    isExtend? enablePlugin() : shrinkPlugin();  
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // const isExtend = request.action === "extend";
+  console.log(request);
+  const level = request.level;
+  controlPictureArea(level);
 });
 
 // transition 설정 함수
 const setTransition = (element) => {
-    if (element && !element.style.transition) {
-        element.style.transition = 'width 0.3s ease-in-out';
-    }
+  if (element && !element.style.transition) {
+    element.style.transition = "width 0.3s ease-in-out";
+  }
 };
 
+const getWidth = (max, min, level) => {
+  const one = (max - min) / (MAX_LEVEL - 1);
+  return min + one * (level - 1);
+};
+
+const controlPictureArea = (level) => {
+  console.log(level);
+  const $entryIframe = document.getElementById("entryIframe");
+  if (!$entryIframe) return;
+
+  const $sub_panel = document.getElementById("sub_panel");
+  const $app_layout = document.getElementById("app-layout");
+  const $left_panel = document.getElementById("section_content");
+
+  const appLayoutWidth = $app_layout.offsetWidth;
+  const leftPanelWidth = $left_panel.offsetWidth;
+
+  if ($sub_panel) {
+    setTransition($sub_panel);
+
+    const max = appLayoutWidth - leftPanelWidth;
+    const width = getWidth(max, MIN_WIDTH, level);
+    $sub_panel.style.width = width + "px";
+  } else {
+    setTransition($entryIframe);
+    const max = appLayoutWidth;
+    const width = getWidth(max, leftPanelWidth, level);
+    $entryIframe.style.width = width + "px";
+  }
+};
 const enablePlugin = () => {
-    const $entryIframe = document.getElementById('entryIframe');
-    if(!$entryIframe) return;
-    
-    const $sub_panel = document.getElementById('sub_panel');
-    const $app_layout = document.getElementById('app-layout');
-    const $left_panel = document.getElementById('section_content');
+  const $entryIframe = document.getElementById("entryIframe");
+  if (!$entryIframe) return;
 
-    const appLayoutWidth = $app_layout.offsetWidth;
+  const $sub_panel = document.getElementById("sub_panel");
+  const $app_layout = document.getElementById("app-layout");
+  const $left_panel = document.getElementById("section_content");
 
-    if($sub_panel){
-        setTransition($sub_panel);
-        const leftPanelWidth = $left_panel.offsetWidth;
-        const subPanelWidth = appLayoutWidth - leftPanelWidth;
-        $sub_panel.style.width = subPanelWidth + 'px';
-    }else{
-        setTransition($entryIframe);
-        $entryIframe.style.width = appLayoutWidth + 'px';
-    }
-    
-}
+  const appLayoutWidth = $app_layout.offsetWidth;
+
+  if ($sub_panel) {
+    setTransition($sub_panel);
+    const leftPanelWidth = $left_panel.offsetWidth;
+    const subPanelWidth = appLayoutWidth - leftPanelWidth;
+    $sub_panel.style.width = subPanelWidth + "px";
+  } else {
+    setTransition($entryIframe);
+    $entryIframe.style.width = appLayoutWidth + "px";
+  }
+};
 
 const shrinkPlugin = () => {
-    const $sub_panel = document.getElementById('sub_panel');
-    const $entryIframe = document.getElementById('entryIframe');
-    
-    if($sub_panel){
-        setTransition($sub_panel);
-        $sub_panel.style.width = '405px';
-    }else{
-        setTransition($entryIframe);
-        $entryIframe.style.width = '100%';
-    }
-}
+  const $sub_panel = document.getElementById("sub_panel");
+  const $entryIframe = document.getElementById("entryIframe");
+
+  if ($sub_panel) {
+    setTransition($sub_panel);
+    $sub_panel.style.width = "405px";
+  } else {
+    setTransition($entryIframe);
+    $entryIframe.style.width = "100%";
+  }
+};
--- popup.js
+++ popup.js
@@ -1,20 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const naverMapExtendToggleButton = document.getElementById('naverMapExtendToggleButton');
-    const naverMapShrinkToggleButton = document.getElementById('naverMapShrinkToggleButton');
+document.addEventListener("DOMContentLoaded", function () {
+  const naverMapExtendToggleButton = document.getElementById(
+    "naverMapExtendToggleButton"
+  );
+  const naverMapShrinkToggleButton = document.getElementById(
+    "naverMapShrinkToggleButton"
+  );
+  const level = document.getElementById("level");
+  let levelValue = 1;
 
-    naverMapExtendToggleButton.addEventListener('click', function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-                action: 'extend'
-            });
+  // 저장된 levelValue 불러오기
+  chrome.storage.local.get(["levelValue"], function (result) {
+    if (result.levelValue) {
+      levelValue = result.levelValue;
+      level.textContent = `${levelValue}`;
+      // 팝업이 열릴 때 현재 단계로 메시지 전송(선택)
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          level: levelValue,
         });
+      });
+    } else {
+      level.textContent = `${levelValue}`;
+    }
+  });
+
+  const sendLevel = (levelValue) => {
+    level.textContent = `${levelValue}`;
+    chrome.storage.local.set({ levelValue }); // 저장
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        level: levelValue,
+      });
     });
+  };
 
-    naverMapShrinkToggleButton.addEventListener('click', function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-                action: 'shrink'
-            });
-        });
-    }); 
-}); 
+  naverMapExtendToggleButton.addEventListener("click", function () {
+    if (levelValue >= 5) return;
+    levelValue++;
+    sendLevel(levelValue);
+  });
+
+  naverMapShrinkToggleButton.addEventListener("click", function () {
+    if (levelValue <= 1) return;
+    levelValue--;
+    sendLevel(levelValue);
+  });
+});
