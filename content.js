3ed1276aea4074054c82e2975f0a1e3e76e798d6
gentlee <gentlee.dev@gmail.com>
2025-07-09 23:24:40 +0900

    feat : 확장단계 도입

--- content.js
+++ content.js
@@ -2,9 +2,7 @@ const MAX_LEVEL = 5;
 const MIN_WIDTH = 405;
 
 // 메시지 리스너
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // const isExtend = request.action === "extend";
-  console.log(request);
+chrome.runtime.onMessage.addListener(function (request) {
   const level = request.level;
   controlPictureArea(level);
 });
@@ -17,12 +15,11 @@ const setTransition = (element) => {
 };
 
 const getWidth = (max, min, level) => {
-  const one = (max - min) / (MAX_LEVEL - 1);
-  return min + one * (level - 1);
+  const oneStep = (max - min) / (MAX_LEVEL - 1);
+  return min + oneStep * (level - 1);
 };
 
 const controlPictureArea = (level) => {
-  console.log(level);
   const $entryIframe = document.getElementById("entryIframe");
   if (!$entryIframe) return;
 
@@ -44,38 +41,5 @@ const controlPictureArea = (level) => {
     const max = appLayoutWidth;
     const width = getWidth(max, leftPanelWidth, level);
     $entryIframe.style.width = width + "px";
-  }
-};
-const enablePlugin = () => {
-  const $entryIframe = document.getElementById("entryIframe");
-  if (!$entryIframe) return;
-
-  const $sub_panel = document.getElementById("sub_panel");
-  const $app_layout = document.getElementById("app-layout");
-  const $left_panel = document.getElementById("section_content");
-
-  const appLayoutWidth = $app_layout.offsetWidth;
-
-  if ($sub_panel) {
-    setTransition($sub_panel);
-    const leftPanelWidth = $left_panel.offsetWidth;
-    const subPanelWidth = appLayoutWidth - leftPanelWidth;
-    $sub_panel.style.width = subPanelWidth + "px";
-  } else {
-    setTransition($entryIframe);
-    $entryIframe.style.width = appLayoutWidth + "px";
-  }
-};
-
-const shrinkPlugin = () => {
-  const $sub_panel = document.getElementById("sub_panel");
-  const $entryIframe = document.getElementById("entryIframe");
-
-  if ($sub_panel) {
-    setTransition($sub_panel);
-    $sub_panel.style.width = "405px";
-  } else {
-    setTransition($entryIframe);
-    $entryIframe.style.width = "100%";
   }
 };
